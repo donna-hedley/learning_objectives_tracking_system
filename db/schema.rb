9230d92d33f348fe02d244896d4c7eb3bb68cad1
Donna Hedley <donna.hedley@shopify.com>
2019-11-09 23:33:36 -0500

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_030611) do
+ActiveRecord::Schema.define(version: 2019_11_10_042232) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_11_10_030611) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
